ff1fcf8560dd353df8f8e3595b3bb836557bb095
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-10 19:29:47 +0530

    added modules to exclude

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from cx_Freeze import setup, Executable
 
 files = ['backend.py', 'gui.qml']
 packages = ["sys", "PySide6.QtGui", "PySide6.QtQml", "PySide6.QtCore", "random", "array"]
-exclude = ["tkinter", "asyncio", "concurrent", "ctypes", "distutils", 
-           "email", "html", "http", "lib2to3", "multiprocessing", 
-           "pydoc_data", "test", "unittest", "xml", "xmlrpc"]
+exclude = ["tkinter", "asyncio", "concurrent", "ctypes", "distutils", "email", "html", "http", "lib2to3",
+           "multiprocessing", "pydoc_data", "test", "unittest", "xml", "xmlrpc"]
+
 
 build_exe_options = {
     "packages": packages,
@@ -17,13 +17,13 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-target = Executable (
+target = Executable(
     script="main.py",
     base=base,
     icon="icon.ico"
 )
 
-setup (
+setup(
     name="PassGen",
     version="1",
     description="Simple app for generating strong passwords",
