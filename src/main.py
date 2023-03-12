04f78bfa4f12b1e16097fa75c3d5da5ded9255c0
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-12 10:47:27 +0530

    switched from QML based gui to pyside6 only gui

--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
+from src.gui import GUI
+from PySide6.QtWidgets import QApplication
 import sys
 
-from PySide6.QtGui import QGuiApplication, QIcon
-from PySide6.QtQml import QQmlApplicationEngine
-
-from backend import Backend
-
-app = QGuiApplication(sys.argv)
-app.setWindowIcon(QIcon(r"icon.ico"))
-
-backend = Backend()
-
-engine = QQmlApplicationEngine()
-engine.rootContext().setContextProperty("backend", backend)
-engine.quit.connect(app.quit)
-engine.load('gui.qml')
+app = QApplication(sys.argv)
+    
+window = GUI(app)
+window.show()
 
 sys.exit(app.exec())
