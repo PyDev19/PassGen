60f024715abd509d2d85ac04e464f677500fbe69
PyDev19 <atharvamishra956+1@gmail.com>
2023-03-12 13:54:48 +0530

    fixed path related errors and bugs

--- src/compile.py
+++ src/compile.py
@@ -1,10 +1,11 @@
 import py_compile
 
-files = ["main.py", "backend.py"]
-output_dir = "../compiled_files"
+files = ["src/main.py", "src/backend.py", "src/gui.py"]
+output_dir = "compiled_files"
 
 for file in files:
-    py_compile.compile(file, cfile=f"{output_dir}/{file.split('.')[0]}.pyc")
+    output_file = file.split('/')[1]
+    py_compile.compile(file, cfile=f"{output_dir}/{output_file.split('.')[0]}.pyc")
     print(f"compiled {file} -> {file.split('.')[0]}.pyc")
 
 print("compiled all files")
